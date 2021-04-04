6ab032df8a25c7246101d067c92719d586ea556e
tkrtmy <tkrtmy1333@gmail.com>
2021-04-04 20:15:02 +0900

    Androidのbackgroundで通知を受け取りたい

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'app.dart';
 
-void main() {
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  print('Handling a background message: ${message.messageId}');
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
